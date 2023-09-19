75d2ec55d35147bc3151b555ee315e014708dd1d
Christine Wong <jumiryoo@gmail.com>
2023-09-18 21:18:47 -0700

    update on serializers.py

--- cheese/serializers.py
+++ cheese/serializers.py
@@ -1,5 +1,4 @@
 from .models import Cheese
-from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
 class CheeseSerializer(serializers.HyperlinkedModelSerializer):
